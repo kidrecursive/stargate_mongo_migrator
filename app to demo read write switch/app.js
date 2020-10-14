2a2455480e9b0bb0b75ec0d48c5d248965a23e3a
sourav11b <souravbiswas@gmail.com>
2020-10-13 18:22:22 -0600

    node js cleaned up

--- app to demo read write switch/app.js
+++ app to demo read write switch/app.js
@@ -1,187 +1,67 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
 
-const assert = require("assert");
-const faker = require("faker");
-const stargate = require("./stargate");
-const _ = require("lodash");
-
-const db_target = "MONGO";
-// setup envars
-require("dotenv").config();
-
+const mongo_dao = require("./mongo_dao");
+const stargate_dao = require("./stargate_dao");
 const fs = require('fs');
+
+const db_target = "DUAL";
+//const db_target = "MONGO";
+//const db_target = "STARGATE";
+
+//const db_operation = "READ";
+//const db_operation = "INSERT";
+//const db_operation = "UPDATE";
+//const db_operation = "DELETE";
+const db_operation = "READ";
 
 var rawdata = fs.readFileSync('jsonfiles/airline_10.json');
 var airline_insert = JSON.parse(rawdata);
-var docid = 10;
-airline_insert._id=docid;
-
-//console.log("###########"+airline_insert);
+var airline_id = 10;
 
 rawdata = fs.readFileSync('jsonfiles/airline_10_update.json');
 var airline_update = JSON.parse(rawdata);
 
-//console.log(airline_update);
-
-
-const namespace = `mongo_migrator`;
-const collection = "airlines";
-
-const gameId = faker.random.alphaNumeric(8);
-const docRootPath = `/namespaces/${namespace}/collections/${collection}/${docid}`;
-
-/*
-var myobj = {
-    gameCode: "DANG",
-    currentState: {
-        name: "ADD_PLAYERS",
-        roundId: null,
-    },
-    players: {},
-    audienceSize: 0,
-    rounds: {
-        1: {
-            type: "QUESTION",
-            title: "Round 1",
-            scoreMultiplier: 1,
-        },
-        2: {
-            type: "QUESTION",
-            title: "Round 1",
-            scoreMultiplier: 2,
-        },
-        3: {
-            type: "COMIC",
-            title: "Final Round",
-            scoreMultiplier: 3,
-        },
-    },
-    questions: {
-        [faker.random.alphaNumeric(8)]: {
-            roundId: 1,
-            content: "What time is it?",
-        },
-        [faker.random.alphaNumeric(8)]: {
-            roundId: 2,
-            content: "What day is it?",
-        },
-        [faker.random.alphaNumeric(8)]: {
-            roundId: 3,
-            content: "https://xkcd.com/386/",
-        },
-    },
-    answers: {},
-    votes: {},
-    audienceVotes: {},
-};
-
-
-var updateDoc = {
-    CRW: {
-        name: "CRW",
-        vip: true,
-        score: 0,
-    },
-    DKG: {
-        name: "DKG",
-        score: 0,
-    },
-    JRG: {
-        name: "JRG",
-        score: 0,
-    },
-    DOG: {
-        name: "DOG",
-        score: 0,
-    },
-};
-*/
-
-// if (db_target.equal("MONGO)")) {
-// }
-
-MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-
-
-    var dbo = db.db("mydb");
-
-    dbo.collection("airlines").deleteMany({}, function (err, result) {
-        if (err) throw err;
-       // console.log(result);
-
-       
-   //     db.close();
-    });
-
-    dbo.collection("airlines").insertOne(airline_insert, function (err, res) {
-        if (err) throw err;
-        console.log("1 document inserted");
-   //    db.close();
-    });
-
-
-    var myquery = { "id": docid };
-   // console.log ("---"+myquery)
-    dbo.collection("airlines").updateOne(myquery, { $set: airline_update }, function (err, res) {
-        if (err) throw err;
-        console.log("1 document updated");
-     //   
-    });
-
-       dbo.collection("airlines").findOne({}, function (err, result) {
-    if (err) throw err;
-   console.log(result);});
-   
-   
-    db.close();
-    
- 
 
 
 
-});
+
+if (db_target == "MONGO") {
+    if (db_operation == "INSERT") {
+        mongo_dao.insert(airline_insert, airline_id)
+    }
+    else if (db_operation == "READ") {
+        mongo_dao.find(airline_id)
+    }
+    else if (db_operation == "UPDATE") {
+        mongo_dao.update(airline_id, airline_update)
+    }
+    else if (db_operation == "DELETE") {
+        mongo_dao.delete_document(airline_id)
+    }
+}
+else if (db_target == "STARGATE") {
+
+    if (db_operation == "INSERT") {
+        stargate_dao.insert(airline_insert, airline_id)
+    }
+    else if (db_operation == "READ") {
+        stargate_dao.find(airline_id)
+    }
+    else if (db_operation == "UPDATE") {
+        stargate_dao.update(airline_id, airline_update)
+    }
+    else if (db_operation == "DELETE") {
+        stargate_dao.delete_document(airline_id)
+    }
+}
+else {
+    console.log("ERROR : UNKNOWN TARGET DB")
+}
 
 
 
-const stargateClient = async () => {
-    const stargateClient = await stargate.createClient({
-      baseUrl: `https://c72c60e1-d1c1-4730-8d6a-413abce921ff-us-east-1.apps.astra.datastax.com`,
-      username: `mongo_migrator`,
-      password: `mongo_migrator`,
-    });
-
-    return stargateClient;
-  }
 
 
-  (async () => {
-     let insert_client = await stargateClient();
-     //console.log(insert_client)
-     await insert_client.delete(docRootPath);
-
-     await insert_client.put(docRootPath,airline_insert );
-  
-    const insert_res = await insert_client.get(docRootPath);
-    console.log("After insert : " + JSON.stringify(insert_res.jsonResponse))
-     await insert_client.patch(docRootPath,airline_update);
-    const update_res = await insert_client.get(docRootPath);
-    console.log("After update : " +JSON.stringify(update_res.jsonResponse))
-  })()
 
 
-/*
-("should add players to a game", async () => {
-    await stargateClient.put(`${docRootPath}/players`, updateDoc);
 
-    const res = await stargateClient.get(`${docRootPath}/players`);
-    assert.equal(res.jsonResponse.data.CRW.name, "CRW");
-    assert.equal(res.jsonResponse.data.DOG.score, 0);
-});
-*/
-
-
-//migration of existing mongo
-//ease of migration from mongo to cassandra
 
